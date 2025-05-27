6cac94907b81ab29ffab4c1a99d0e82804438615
wellingtonpc <well_pcosta@hotmail.com>
2025-05-27 15:31:47 -0300

    Add HTML and JavaScript files for DOM and Objects implementations

---
+++ javascript/objects/script.js
@@ -0,0 +1,29 @@
+var Person = {
+    name: "John",
+    age: 30
+};
+
+var Square = {
+    side: 5,
+    area: function() {
+        return this.side * this.side;
+    },
+    perimeter: function() {
+        return 4 * this.side;
+    }
+};   
+console.log(Math.random()); // Generates a random number between 0 and 
+
+// operador ternário
+var idade;
+var maiorIdade = (idade > 18) ? "Adulto": "Menor de idade";
+
+
+const num2 = 5;
+function sumTwoNumbers(num1) {
+    return num1 + num2;
+}
+
+function division(num1) {
+    return num1 / num2;
+}
